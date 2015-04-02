70b0f7eaf87440689430606bf97980e56d8ae3a8
Tom Zmyslo <tzmyslo25@gmail.com>
2015-04-02 17:28:42 -0400

    Added jQuery & Bootstrap

--- js/system.js
+++ js/system.js
@@ -2,23 +2,20 @@ window.onload = function() {
   
   chrome.system.cpu.getInfo(
     function(info) {
-      var a = document.getElementById("processor");
-      a.innerHTML = info.modelName;
+      $("#processor").html(info.modelName);
     }
   );
   
   chrome.system.memory.getInfo(
     function(info) {
-      var a = document.getElementById("memory");
-      var x = (((info.capacity / 1024) / 1024) / 1024)
-      a.innerHTML = x + " GB";
+      var capacity = (((info.capacity / 1024) / 1024) / 1024)
+      $("#memory").html(capacity + " GB RAM");
     }
   );
   
   chrome.system.network.getNetworkInterfaces(
     function(info) {
-      var a = document.getElementById("network");
-      a.innerHTML = info[0].name + " @ " + info[0].address;
+      $("#network").html(info[0].name + " @ " + info[0].address);
     }
   );
   
